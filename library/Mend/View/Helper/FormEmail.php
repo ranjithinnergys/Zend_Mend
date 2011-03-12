8b4f696bc2ba800f5d1a01c98fe29ae8a1daea19
Doug Hurst <dalan.hurst@gmail.com>
2011-03-12 17:53:09 -0600

    Created new FormEmail helper which extends FormHtml5Abstract view helper

---
+++ library/Mend/View/Helper/FormEmail.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Zend_Mend
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ *
+ * @category   Zend_Mend
+ * @package    Form
+ * @subpackage Element
+ * @license    http://www.opensource.org/licenses/bsd-license New BSD License
+ */
+
+/**
+ * HTML5 Form Email Element View Helper
+ *
+ * @since Feb 19, 2011
+ * @author Doug Hurst <dalan.hurst@gmail.com>
+ */
+abstract class Mend_View_Helper_FormEmail extends Mend_View_Helper_FormHtml5Abstract
+{
+    /** @staticvar HTML5_TYPE input[type] introduced by HTML5 **/
+    const HTML5_TYPE = 'email';
+
+    /** @staticvar Legacy fallback type **/
+    const FALLBACK_TYPE = 'text';
+
+    /**
+     * Generates an input type='email' element
+     *
+     * @param string|array $name If a string, the element name.  If an
+     * array, all other parameters are ignored, and the array elements
+     * are used in place of added parameters.
+     *
+     * @param mixed $value The element value.
+     *
+     * @param array $attribs Attributes for the element tag.
+     *
+     * @return string The element HTML.
+     */
+    public function formEmail($name, $value = null, $attribs = null)
+    {
+        return parent::formHtml5($name, $value, $attribs);
+    }
+}
--- library/Mend/View/Helper/FormHtml5Abstract.php
+++ library/Mend/View/Helper/FormHtml5Abstract.php
@@ -15,17 +15,21 @@
  */
 
 /**
- * Email Form Element Helper
+ * HTML5 Form Element Input Abstract Helper
  *
  * @since Feb 19, 2011
  * @author Doug Hurst <dalan.hurst@gmail.com>
  */
-class Mend_View_Helper_FormEmail extends Zend_View_Helper_FormElement
+abstract class Mend_View_Helper_FormHtml5Abstract extends Zend_View_Helper_FormElement
 {
+    /** @staticvar HTML5_TYPE input[type] introduced by HTML5 **/
+    const HTML5_TYPE = 'text';
+
+    /** @staticvar Legacy fallback type **/
+    const FALLBACK_TYPE = 'text';
+
     /**
-     * Generates an input type='email' element.
-     *
-     * @access public
+     * Generates an input type='email' element
      *
      * @param string|array $name If a string, the element name.  If an
      * array, all other parameters are ignored, and the array elements
@@ -35,29 +39,36 @@ class Mend_View_Helper_FormEmail extends Zend_View_Helper_FormElement
      *
      * @param array $attribs Attributes for the element tag.
      *
-     * @return string The element (X)HTML.
+     * @return string The element HTML.
      */
-    public function formEmail($name, $value = null, $attribs = null)
+    public function formHtml5($name, $value = null, $attribs = null)
     {
         $info = $this->_getInfo($name, $value, $attribs);
-        extract($info); // name, value, attribs, options, listsep, disable
+        extract($info);
 
-        // build the element
+        /** Set disabled property */
         $disabled = '';
-        if ($disable) {
-            // disabled
-            $disabled = ' disabled="disabled"';
-        }
+        if ($disable) $disabled = ' disabled="disabled"';
 
-        // XHTML or HTML end tag?
+        /** Set correct end tag for XHTML or HTML */
         $endTag = ' />';
-        $inputType = 'text';
-        if (($this->view instanceof Zend_View_Abstract) && !$this->view->doctype()->isXhtml()) {
-            $endTag= '>';
-        }
-        if(($this->view instanceof Zend_View_Abstract) && $this->view->doctype()->isHtml5()) $inputType = 'email';
+        if (
+            $this->view instanceof Zend_View_Abstract &&
+            !$this->view->doctype()->isXhtml()
+        ) $endTag = '>';
 
-        $xhtml = '<input type="'.$inputType.'"'
+        /**
+         * Set input[type]
+         *
+         * Will fallback to input[type=text] if the doctype is not HTML5
+         */
+        $inputType = self::FALLBACK_TYPE;
+        if(
+            $this->view instanceof Zend_View_Abstract &&
+            $this->view->doctype()->isHtml5()
+        ) $inputType = self::HTML5_TYPE;
+
+        $html = '<input type="'.$inputType.'"'
                 . ' name="' . $this->view->escape($name) . '"'
                 . ' id="' . $this->view->escape($id) . '"'
                 . ' value="' . $this->view->escape($value) . '"'
@@ -65,6 +76,6 @@ class Mend_View_Helper_FormEmail extends Zend_View_Helper_FormElement
                 . $this->_htmlAttribs($attribs)
                 . $endTag;
 
-        return $xhtml;
+        return $html;
     }
 }
