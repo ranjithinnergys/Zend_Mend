dd2d3483107a30e92691c9f682882af9f2711254
Doug Hurst <dalan.hurst@gmail.com>
2011-04-23 23:14:07 -0500

    Enabled classmap autoloading in tests, added Phing target for PHP AutoBuilder CLI

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -18,20 +18,9 @@ error_reporting(E_ALL | E_STRICT);
 //  Maximize memory for testing
 ini_set('memory_limit', '-1');
 
-//  Ensure library/ is on include_path
-set_include_path(
-    implode(
-        PATH_SEPARATOR,
-        array(
-            realpath(realpath(dirname(__FILE__) . '/../library')),
-            realpath('/usr/local/zend/share/ZendFramework/library'),
-            get_include_path()
-        )
-    )
-);
-
-//  Start autoloader
-require_once 'Zend/Loader/Autoloader.php';
-$loader = Zend_Loader_Autoloader::getInstance();
-$loader->registerNamespace('Mend_');
+//  Class-map Autoloading
+//  @link http://weierophinney.net/matthew/archives/245-Autoloading-Benchmarks.html
+require_once __DIR__.'/../resources/classmap.Zend_Mend.php';
+require_once __DIR__.'/../resources/classmap.Zend.php';
+require_once __DIR__.'/../resources/classmap.ZendX.php';
 
