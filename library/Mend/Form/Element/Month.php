c04ce01b68581c34f1646e76d94a71ec5c238312
Doug Hurst <dalan.hurst@gmail.com>
2011-04-24 09:45:08 -0500

    Added remaining HTML5 input types

---
+++ library/Mend/Form/Element/Month.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Mend Library
+ *
+ * PHP version 5.3
+ *
+ * @category   Zend_Mend
+ * @package    Controllers
+ * @subpackage Plugins
+ * @author     Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright  2011 Doug Hurst
+ * @license    http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link       http://github.com/dalanhurst/Zend_Mend
+ */
+
+/**
+ * HTML5 Month Form Element
+ *
+ * @category   Zend_Mend
+ * @package    Forms
+ * @subpackage Elements
+ * @author     Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright  2011 Doug Hurst
+ * @license    http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link       http://dev.w3.org/html5/markup/input.month.html
+ * @link       http://github.com/dalanhurst/Zend_Mend
+ */
+class Mend_Form_Element_Month extends Zend_Form_Element_Xhtml
+{
+    /**
+     * Default form view helper to use for rendering
+     * @var string
+     */
+    public $helper = 'formMonth';
+}
---
+++ library/Mend/View/Helper/FormMonth.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Mend Library
+ *
+ * PHP version 5.3
+ *
+ * @category   Zend_Mend
+ * @package    View
+ * @subpackage Helpers
+ * @author     Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright  2011 Doug Hurst
+ * @license    http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link       http://github.com/dalanhurst/Zend_Mend
+ */
+
+/**
+ * HTML5 Form Month Element View Helper
+ *
+ * @category   Zend_Mend
+ * @package    View
+ * @subpackage Helpers
+ * @author     Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright  2011 Doug Hurst
+ * @license    http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link       http://github.com/dalanhurst/Zend_Mend
+ */
+class Mend_View_Helper_FormMonth extends Mend_View_Helper_FormHtml5Abstract
+{
+    /** @staticvar HTML5_TYPE input[type] introduced by HTML5 **/
+    const HTML5_TYPE = 'month';
+
+    /**
+     * Generates an input type='month' element
+     *
+     * @param string|array $name    If a string, the element name.  If an
+     * 	array, all other parameters are ignored, and the array elements
+     * 	are used in place of added parameters
+     * @param mixed        $value   The element value
+     * @param array        $attribs Attributes for the element tag
+     *
+     * @return string The element HTML.
+     */
+    public function formMonth($name, $value = null, $attribs = null)
+    {
+        return parent::formHtml5($name, $value, $attribs);
+    }
+}
