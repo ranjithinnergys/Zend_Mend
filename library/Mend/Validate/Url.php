705608348b5375f933dbed74a05c5c55832cebcd
Doug Hurst <dalan.hurst@gmail.com>
2011-07-15 18:54:24 -0500

    URL Validator

---
+++ library/Mend/Validate/Url.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Mend Library
+ *
+ * PHP version 5.3
+ *
+ * @category  Zend_Mend
+ * @package   Util
+ * @author    Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright 2011 Doug Hurst
+ * @license   http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link      http://github.com/dalanhurst/Zend_Mend
+ */
+
+/**
+ * URL Validator
+ *
+ * @category  Zend_Mend
+ * @package   Validators
+ * @author    Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright 2011 Doug Hurst
+ * @license   http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link      http://github.com/dalanhurst/Zend_Mend
+ */
+class Mend_Validate_Url extends Zend_Validate_Abstract
+{
+    /**
+     * Returns true if and only if $value meets the validation requirements
+     *
+     * If $value fails validation, then this method returns false, and
+     * getMessages() will return an array of messages that explain why the
+     * validation failed.
+     *
+     * @param  mixed $value
+     * @return boolean
+     * @throws Zend_Validate_Exception If validation of $value is impossible
+     */
+    public function isValid($value)
+    {
+        return Zend_Uri_Http::check($value);
+    }
+}
