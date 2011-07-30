27a1a6c6a330270ef242d01b96a44edff04c0c82
Doug Hurst <dalan.hurst@gmail.com>
2011-07-29 22:20:02 -0500

    Added an implementation of Enumerations

---
+++ library/Mend/Model/Enum/Abstract.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Mend Library
+ *
+ * PHP version 5.3
+ *
+ * @category  Zend_Mend
+ * @package   Util
+ * @author    Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright 2011 Doug Hurst
+ * @license   http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link      http://github.com/dalanhurst/Zend_Mend
+ */
+
+/**
+ * Abstract Enumeration
+ *
+ * @category  Zend_Mend
+ * @package   Model
+ * @author    Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright 2011 Doug Hurst
+ * @license   http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link      http://github.com/dalanhurst/Zend_Mend
+ */
+abstract class Mend_Model_Enum_Abstract
+implements Mend_Model_Enum_Interface
+{
+    /**
+     * Enum->toArray() using reflection and late static binding
+     *
+     * @return array
+     */
+    final public static function toArray()
+    {
+        assert('is_string(static::getClassname())');
+        $reflection = new ReflectionClass(static::getClassname());
+        return $reflection->getConstants();
+    }
+
+    /**
+     * Disable Construction
+     */
+    final private function __construct()
+    {
+    }
+
+    /**
+     * Disable Magic Accessor
+     */
+    final public function __get($name)
+    {
+    }
+
+    /**
+     * Disable Magic Mutator
+     */
+    final public function __set($name, $value)
+    {
+    }
+}
---
+++ library/Mend/Model/Enum/Interface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Mend Library
+ *
+ * PHP version 5.3
+ *
+ * @category  Zend_Mend
+ * @package   Util
+ * @author    Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright 2011 Doug Hurst
+ * @license   http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link      http://github.com/dalanhurst/Zend_Mend
+ */
+
+/**
+ * Enumeration Interface
+ *
+ * @category  Zend_Mend
+ * @package   Model
+ * @author    Doug Hurst <dalan.hurst@gmail.com>
+ * @copyright 2011 Doug Hurst
+ * @license   http://www.opensource.org/licenses/bsd-license New BSD License
+ * @link      http://github.com/dalanhurst/Zend_Mend
+ */
+interface Mend_Model_Enum_Interface
+{
+    /**
+     * Get the name of this class
+     *
+     * Implemented as an interface because abstract static is not allowed.
+     *
+     * @return string
+     */
+    public static function getClassname();
+}
